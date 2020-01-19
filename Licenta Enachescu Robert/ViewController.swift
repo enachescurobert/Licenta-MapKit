b2850f83bda982b844b4ff5cdd764301d4cf2f92
Enachescu Robert <enachescurobert@gmail.com>
2020-01-19 21:46:05 +0200

    The map wil be centered in Bucharest from now on

--- Licenta Enachescu Robert/ViewController.swift
+++ Licenta Enachescu Robert/ViewController.swift
@@ -8,13 +8,23 @@
 
 import UIKit
 import CoreLocation
+import MapKit
 
 class ViewController: UIViewController {
-
+  
+  // MARK: - IBOutlets
+  @IBOutlet weak var mapView: MKMapView!
+  
   var locationManager: CLLocationManager?
   
   override func viewDidLoad() {
     super.viewDidLoad()
+    
+    let ourLocation = CLLocation(latitude: 44.410, longitude: 26.100)
+    let regionRadius: CLLocationDistance = 25000.0
+    let region = MKCoordinateRegion(center: ourLocation.coordinate, latitudinalMeters: regionRadius, longitudinalMeters: regionRadius)
+    mapView.setRegion(region, animated: true)
+    mapView.delegate = self
 
     locationManager = CLLocationManager()
     locationManager?.delegate = self
@@ -22,17 +32,30 @@ class ViewController: UIViewController {
     locationManager?.allowsBackgroundLocationUpdates = true
     locationManager?.requestLocation()
     
+    
+    startLocationService()
+    
     }
   
-  @IBAction func startLocationService(_ sender: Any) {
+//  MARK: - IBActions
+  @IBAction func changeMapType(_ sender: UISegmentedControl) {
+    if sender.selectedSegmentIndex == 0 {
+      mapView.mapType = .standard
+    } else if sender.selectedSegmentIndex == 1 {
+      mapView.mapType = .satellite
+    }
+  }
+  
+  
+  func startLocationService() {
     locationManager?.requestAlwaysAuthorization()
   }
 
 }
 
-extension ViewController : CLLocationManagerDelegate {
+//  MARK: - CoreLocation Delegate methods
+extension ViewController: CLLocationManagerDelegate {
   
-//  MARK: - Location Delegate methods
   func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
     
     guard let initialLocation = locations.first else {return}
@@ -42,6 +65,15 @@ extension ViewController : CLLocationManagerDelegate {
   
   func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
     print(error)
+  }
+  
+}
+
+//  MARK: - MapKit Delegate methods
+extension ViewController: MKMapViewDelegate {
+  
+  func mapViewWillStartRenderingMap(_ mapView: MKMapView) {
+    print("Rendering...")
   }
   
 }
